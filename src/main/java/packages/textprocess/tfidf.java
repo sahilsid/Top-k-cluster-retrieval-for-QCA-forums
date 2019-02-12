84d4efdfa99a4cf61d678d7916952204a11214f2
sahilsid <sahil.sidheekh@gmail.com>
2019-02-12 20:09:33 +0530

    Similiarity matrix

--- src/main/java/packages/textprocess/tfidf.java
+++ src/main/java/packages/textprocess/tfidf.java
@@ -12,6 +12,7 @@ import org.json.simple.JSONObject;
 public class tfidf {
 
     Map<String, Double> valueMap;
+    Map<String, Double> similiarityMap;
     Map<String, List<String>> data;
 
     public static void main(String[] args) {
@@ -21,6 +22,7 @@ public class tfidf {
     tfidf() {
         data = new HashMap<String, List<String>>();
         valueMap = new HashMap<String, Double>();
+        similiarityMap = new HashMap<String, Double>();
         Preprocess p = new Preprocess();
         JSONObject questions = jsonImport.getjson("src/main/resources/android_questions.json");
         List<String> processed = new ArrayList<String>();
@@ -54,30 +56,39 @@ public class tfidf {
             idf = 0.0;
             List<String> keywords = data.get(qid);
             tf = tf + Collections.frequency(keywords, word);
-            tf = tf / keywords.size();
-        
+
             for (String qids : data.keySet()) {
                 if (data.get(qids).contains(word))
                     idf = idf + 1;
             }
-            idf = idf / data.size();
+            idf = Math.log(data.size() / idf);
             valueMap.replace(key, tf * idf);
         }
-        System.out.println(valueMap);
-
+        generateSimiliarityMap();
     }
-    Double similiarity(String q1, String q2){
-        List<String> common_words = data.get(q1);
+
+    Double similiarity(String q1, String q2) {
+        List<String> common_words = new ArrayList<String>(data.get(q1));
         common_words.retainAll(data.get(q2));
-        Double psum = 0.0,i_sum_1=0.0,i_sum_2=0.0;
-        for(String common_word : common_words){
-            i_sum_1 = i_sum_1 + valueMap.get(q1+"."+common_word);
-            i_sum_2 = i_sum_2 + valueMap.get(q2+"."+common_word);
-            psum = psum + i_sum_1*i_sum_2;
+        Double psum = 0.0, i_sum_1 = 0.0, i_sum_2 = 0.0;
+        for (String common_word : common_words) {
+            i_sum_1 = i_sum_1 + valueMap.get(q1 + "." + common_word);
+            i_sum_2 = i_sum_2 + valueMap.get(q2 + "." + common_word);
+            psum = psum + i_sum_1 * i_sum_2;
         }
         i_sum_1 = Math.sqrt(i_sum_1);
         i_sum_2 = Math.sqrt(i_sum_2);
 
-        return (psum/(i_sum_1*i_sum_2));
+        return (i_sum_1 * i_sum_2 > 0) ? psum / (i_sum_1 * i_sum_2) : 0.0;
+    }
+
+    void generateSimiliarityMap() {
+        for (Object qid1 : data.keySet()) {
+            for (Object qid2 : data.keySet()) {
+                if (!similiarityMap.containsKey(qid1 + "." + qid2) && !similiarityMap.containsKey(qid2 + "." + qid1))
+                    similiarityMap.put(qid1 + "." + qid2, similiarity((String) qid1, (String) qid2));
+            }
+        }
+        //System.out.println(similiarityMap);
     }
 }
