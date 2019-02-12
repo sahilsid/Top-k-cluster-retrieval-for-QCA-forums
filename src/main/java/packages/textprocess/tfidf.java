b8f115175cea0393f5f31058ea89004f2dca43e5
sahilsid <sahil.sidheekh@gmail.com>
2019-02-12 11:28:08 +0530

    Tf_Idf

--- src/main/java/packages/textprocess/tfidf.java
+++ src/main/java/packages/textprocess/tfidf.java
@@ -1,21 +1,71 @@
 
 package packages.textprocess;
+
+import java.awt.Container;
 import java.util.*;
+import javafx.util.Pair;
+import org.json.simple.JSONObject;
+
 /**
  * tfidf
  */
 public class tfidf {
-public static void main(String[] args) {
-    List<Dictionary<String,String>> termsfreq = new ArrayList<Dictionary<String,String>>();
-    for(Dictionary<String,String> elem : termsfreq){
 
-       Enumeration key = elem.keys();
+    Map<String, Double> valueMap;
+    Map<String, List<String>> data;
 
-       while(key.hasMoreElements()){
-           System.out.println(elem.get(key.nextElement()));
-       }
+    public static void main(String[] args) {
+        tfidf a = new tfidf();
     }
-    System.out.print("o");
-}
-    
+
+    tfidf() {
+        data = new HashMap<String, List<String>>();
+        valueMap = new HashMap<String, Double>();
+        Preprocess p = new Preprocess();
+        JSONObject questions = jsonImport.getjson("src/main/resources/android_questions.json");
+        List<String> processed = new ArrayList<String>();
+
+        for (Object key : questions.keySet()) {
+            String k = (String) key;
+            JSONObject value = (JSONObject) questions.get(key);
+            processed = p.keywords((String) value.get("body"));
+            data.put(k, processed);
+        }
+        System.out.println("data : "+data);
+        initialize();
+        // System.out.println(data);
+    }
+
+    void initialize() {
+        for (String qid : data.keySet()) {
+            for (String keywords : data.get(qid)) {
+                valueMap.putIfAbsent(qid + "." + keywords, 0.0);
+            }
+        }
+        assignTfIdf();
+    }
+
+    void assignTfIdf() {
+        for (String key : valueMap.keySet()) {
+            StringTokenizer token = new StringTokenizer(key, ".");
+            String qid = token.nextToken();
+            String word = token.nextToken();
+            System.out.println(qid + " and " + word);
+            Double tf, idf;
+            tf = 0.0;
+            idf = 0.0;
+            List<String> keywords = data.get(qid);
+            tf = tf + Collections.frequency(keywords, word);
+            tf = tf / keywords.size();
+        
+            for (String qids : data.keySet()) {
+                if (data.get(qids).contains(word))
+                    idf = idf + 1;
+            }
+            idf = idf / data.size();
+            valueMap.replace(key, tf * idf);
+        }
+        System.out.println(valueMap);
+
+    }
 }
